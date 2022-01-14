04dbcf7c19a93908e9aaba8b3fe559528bd4b75f
IgorTimo <rostovplace@yandex.ru>
2022-01-14 14:13:45 +0300

    instal mongodb and add comment page

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
 import express from "express";
 import hbs from "hbs";
 import path from "path";
+import mongoose from "mongoose";
 
 const __dirname = path.resolve();
 const app = express();
 const users = [
-    { id: 1, name: "Ivan", surname: "Petrov", desc: "Somethig abou ivan" },
-    {
-      id: 2,
-      name: "Bill",
-      surname: "Gates",
-      desc: "Founder of Microsoft",
-      age: 65,
-    },
-  ];
+  { id: 1, name: "Ivan", surname: "Petrov", desc: "Somethig abou ivan" },
+  {
+    id: 2,
+    name: "Bill",
+    surname: "Gates",
+    desc: "Founder of Microsoft",
+    age: 65,
+  },
+];
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+await mongoose.connect("mongodb://localhost/mcs");
+
+const Schema = mongoose.Schema;
+const CommentSchema = new Schema({
+  title: String,
+  body: String,
+  date: Date,
+});
+const Comment = mongoose.model("Comment", CommentSchema);
 
 app.get("/", (req, res) => {
   res.render("index", { main_title: "Node Express server" });
+});
+
+app.get("/comments", (req, res) => {
+    Comment.find().then((comments) => {
+        res.render("comments/index", { main_title: "Comments", comments: comments });
+    }).catch(err => {
+        es.status(400).json({ e: err });
+    })
+  
+});
+
+app.post("/comments", (req, res) => {
+  const comment = new Comment({
+    title: req.body["comment_title"],
+    body: req.body["comment_body"],
+    date: new Date(),
+  });
+  comment
+    .save()
+    .then(() => {
+      res.redirect("/comments");
+    })
+    .catch((e) => {
+      res.status(400).json({ e: e });
+    });
 });
 
 app.get("/about", (req, res) => {
@@ -30,12 +65,10 @@ app.get("/users", (req, res) => {
 });
 
 app.get("/users/:user_id", (req, res) => {
-    const user = users.find(user => user["id"] === parseInt(req.params['user_id']))
-    res.render("user", { main_title: user.name, user: user });
-});
-
-app.post("/search", (req, res) => {
-  res.send(req.body);
+  const user = users.find(
+    (user) => user["id"] === parseInt(req.params["user_id"])
+  );
+  res.render("user", { main_title: user.name, user: user });
 });
 
 app.listen(3000, () => {
